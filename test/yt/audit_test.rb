b8b7c0a7083cf8e2a796abe01310105c65826373
Kang-Kyu Lee <kangkyu1111@gmail.com>
2016-02-29 14:53:04 -0800

    break test with Hollywood Game Night

--- test/yt/audit_test.rb
+++ test/yt/audit_test.rb
@@ -3,6 +3,7 @@ require 'yt/audit'
 
 class Yt::AuditTest < Minitest::Test
   def test_channel_audit
+    skip
     audit = Yt::Audit.new(channel_id: 'UCKM-eG7PBcw3flaBvd0q2TQ')
     result = audit.run
 
@@ -29,5 +30,13 @@ class Yt::AuditTest < Minitest::Test
     assert_equal 'Playlist Description', result[5].title
     assert_equal 1, result[5].valid_count
     assert_equal 2, result[5].total_count
+  end
+
+  def test_youtube_association_of_HollywoodGameNight
+    audit = Yt::Audit.new(channel_id: 'UCpBkBMO8IbYAMdtYNaMxKgQ')
+    result = audit.run
+    assert_equal 'YouTube Association', result[3].title
+    assert_equal 10, result[3].valid_count
+    assert_equal 10, result[3].total_count
   end
 end
